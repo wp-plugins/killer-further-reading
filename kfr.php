2bee226cf5b246c840c2f68ce85c6207c3c19d77
shabbirbhimani <shabbirbhimani@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-27 05:21:46 +0000

    Update
    git-svn-id: https://plugins.svn.wordpress.org/killer-further-reading/trunk@846016 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ kfr.php
@@ -0,0 +1,143 @@
+<?php
+/*
+Plugin Name: Killer Further Reading
+Plugin URI: http://wordpress.org/extend/plugins/killer-further-reading/
+Description: The best content from your blog is shown to the user for further reading only if he has not read the content. The tracking is done through cookie being placed for the content already read.
+Author: Shabbir Bhimani
+Version: 0.02
+Author URI: http://imtips.co/
+*/
+// Creating the widget
+class kfr_widget extends WP_Widget {
+    
+    function __construct() {
+        parent::__construct(
+            // Base ID of your widget
+            'killer_further_reading',
+
+            // Widget name will appear in UI
+            __('Killer Further Reading', 'killer_further_reading'),
+
+            // Widget description
+            array( 'description' => __( 'Killer Further Reading Widget', 'killer_further_reading' ), )
+            );
+    }
+    
+    // Creating widget front-end
+    // This is where the action happens
+    public function widget( $args, $instance ) {
+
+        if ( ! empty( $instance['tag_slug'] ) && is_numeric($instance['post_count']))
+        {
+            $title = apply_filters( 'widget_title', $instance['title'] );
+            // before and after widget arguments are defined by themes
+            echo $args['before_widget'];
+            if ( ! empty( $title ) )
+                echo $args['before_title'] . $title . $args['after_title'];
+            
+            // This is where you run the code and display the output
+            $tag_slug = $instance['tag_slug'];
+            $post_count = $instance['post_count'];
+            _best_of_posts($tag_slug, $post_count);
+        }
+        echo $args['after_widget'];
+    }
+    
+    // Widget Backend
+    public function form( $instance ) {
+        if ( isset( $instance[ 'title' ] ) ) {
+            $title = $instance[ 'title' ];
+        }
+        else {
+            $title = __( 'Further Reading', 'killer_further_reading' );
+        }
+        if ( isset( $instance[ 'tag_slug' ] ) ) {
+            $tag_slug = $instance[ 'tag_slug' ];
+        }
+        else {
+            $tag_slug = __( '', 'killer_further_reading' );
+        }
+        if ( isset( $instance[ 'post_count' ] ) ) {
+            $post_count = $instance[ 'post_count' ];
+        }
+        else {
+            $post_count = __( '5', 'killer_further_reading' );
+        }
+        
+        // Widget admin form
+    ?>
+<p>
+<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
+</p>
+<p>
+<label for="<?php echo $this->get_field_id( 'tag_slug' ); ?>"><?php _e( 'Tag Slug for Further Reading:' ); ?></label>
+<input class="widefat" id="<?php echo $this->get_field_id( 'tag_slug' ); ?>" name="<?php echo $this->get_field_name( 'tag_slug' ); ?>" type="text" value="<?php echo esc_attr( $tag_slug ); ?>" />
+</p>
+<p>
+<label for="<?php echo $this->get_field_id( 'post_count' ); ?>"><?php _e( 'Number of Posts to Show:' ); ?></label>
+<input class="widefat" id="<?php echo $this->get_field_id( 'post_count' ); ?>" name="<?php echo $this->get_field_name( 'post_count' ); ?>" type="text" value="<?php echo esc_attr( $post_count ); ?>" />
+</p>
+    <?php
+    }
+
+    // Updating widget replacing old instances with new
+    public function update( $new_instance, $old_instance ) {
+        $instance = array();
+        $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : 'Further Reading';
+        $instance['tag_slug'] = ( ! empty( $new_instance['tag_slug'] ) ) ? strip_tags( $new_instance['tag_slug'] ) : '';
+        $instance['post_count'] = ( ! is_numeric( $new_instance['post_count'] ) ) ? strip_tags( $new_instance['post_count'] ) : '5';
+        return $instance;
+    }
+} // Class kfr_widget ends here
+
+// Register and load the widget
+function kfr_load_widget() {
+    register_widget( 'kfr_widget' );
+}
+add_action( 'widgets_init', 'kfr_load_widget' );
+
+function _best_of_posts($tag_slug, $number_posts = 5) {
+    echo "<ul class=\"killer-further-reading\">";
+    $excluded = get_the_ID();
+    if(isset($_COOKIE['haveseen']))
+        $excluded = $_COOKIE['haveseen'].','.get_the_ID();
+    $args = array(
+                  'numberposts'     => $number_posts,
+                  'offset'          => 0,
+                  'category'        => 0,
+                  'tag'             => $tag_slug,
+                  'orderby'         => 'post_date',
+                  'order'           => 'RAND',
+                  'include'         => '',
+                  'exclude'         => $excluded,
+                  'meta_key'        => '',
+                  'meta_value'      => '',
+                  'post_type'       => 'post',
+                  'post_mime_type'  => '',
+                  'post_parent'     => '',
+                  'post_status'     => 'publish' );
+    $posts_array = get_posts($args);
+    foreach($posts_array as $eachpost) {
+        echo "<li><a href=\"" . get_permalink($eachpost->ID) . "\" rel=\"nofollow\">" . get_the_title($eachpost->ID) . "</a></li>";
+    }
+    echo "</ul>";
+}
+
+function set_haveseen_cookie()
+{
+    if(is_single())
+    {
+        $seen_posts = array();
+        if(isset($_COOKIE['haveseen']))
+        {
+            $seen_posts = explode(',',$_COOKIE['haveseen']);
+        }
+        $just_seen = get_the_ID();
+        if(!in_array($just_seen,$seen_posts)) {
+            array_push($seen_posts,$just_seen);
+        }
+        setcookie('haveseen', implode(',',$seen_posts), time()+1209600, COOKIEPATH, COOKIE_DOMAIN, false);
+    }
+}
+add_action( 'wp', 'set_haveseen_cookie');
---
+++ readme.txt
@@ -0,0 +1,46 @@
+=== Plugin Name ===
+Contributors: shabbirbhimani
+Tags: Widget, Further Reading, Cookies
+Requires at least: 2.8
+Tested up to: 3.8.1
+Stable tag: trunk
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Killer further reading recommends content to user based on a tag and excludes already visited content.
+
+== Description ==
+
+The plugin adds a widget named Killer Further Reading in your Wordpress widget area and recommends content based on a tag you specify in the widget settings. The killer part is it excludes posts that are already visited by the user. The tracking is done based on cookies and so if you are using the plugin, you may need to include about cookies in privacy notice for your blog.
+
+For blogs that aren't too general, every content is more or less related and so the idea is to show the best or evergreen content for further reading than those that is not as good as those but is more related to what user is reading.
+
+Other further reading plugins just show the best or most popular articles based on tag or category of the article you are currently on but that may not always be the best content of the blog. So if you have a better article from other category but is one of the best resource of your blog, it makes much more sense to be showing that content rather. The killer part helps avoiding the content that is already viewed once and so you can suggest more and more of your best content as the process of reading continues.
+
+== Installation ==
+
+Please Follow the exact Steps to install and activate this plugin.
+
+   1. Upload the plugin folder killer-further-reading at wp-content/plugins.
+   2. Activate the plugin through the 'Plugins' menu in WordPress
+   3. Drag Killer Further Reading to the widget area for options to choose the tag for showing content.
+
+== Frequently Asked Questions ==
+
+= Installed and activated the Plugin but cannot find any option for the plugin? =
+
+The plugin adds a widget and so you will see a new widget Killer Further Reading widget that can be dragged anywhere you prefer like in footer, sidebars or even after post (Provided your theme has a widget area there).
+
+== Screenshots ==
+
+1. Plugin Widget. 
+
+== Changelog ==
+
+= 0.01 =
+* Basic Working Version of Killer Further Reading
+
+== Upgrade Notice ==
+
+= 0.02 =
+Just Replace the content of the plugins folder with the new content and re-activate the plugin.
